f2eb52f8cba8b6b7fab70fb415c199266106507f
sergiuhruban <hrubansergiu@gmail.com>
2022-09-13 22:28:33 +0300

    Initial commit

---
+++ hello_word/main.py
@@ -0,0 +1,105 @@
+# # this is my first python script
+# # print("Hello, World1")
+# print("Hello, World")
+# # print("Hello, World!")
+#
+# print(1, 4.5, 3 + 5j)
+#
+# # type()
+# # id()
+#
+# print(type(1), type(4.5), type(3 + 5j))
+# print(id(1), id(4.5), id(3 + 5j))
+#
+# # int()
+# print(int(2), int(4.5))
+#
+# # float()
+# print(float(2), float(4.5))
+#
+# # complex()
+# print(complex(2), complex(4.5))
+#
+# print(7 is 7)
+#
+# # in python null = none
+#
+# print((5 == 5) is True)
+#
+# # 123456789 34556678989
+# a = 123456789
+# b = 34556678989
+#
+# print(a + b)
+# print(a - b)
+#
+# a = 3
+# print('a', a)
+#
+# print('a', a, id(a), type(a))
+#
+# print('this is a string')
+# print("this is a string")
+#
+# print("that's my dog")
+# print('My father said:"go to work"')
+#
+# #my_string = 'My age is 20.'
+# #print('my_string', my_string)
+#
+
+# my_age = 15
+#
+# if my_age >= 18:
+#     print('Instr. 1: persoana este adulta.')
+#     print('Instr. 2: persoana este adulta.')
+#
+# else:
+#
+#     if my_age >= 14:
+#         print('Instr. 1: persoana este minora, dar are buletin.')
+#     else:
+#         print('Instr. 1: persoana este minora, dar are buletin.')
+#
+# print('End of program')
+
+# print('before while')
+#
+# index = 0
+#
+# while index < 10:
+#     print('inside while', index)
+#     index += 1
+#
+# print('after while')
+
+# print('before for')
+#
+# for element in [1,2, 3, 4, 5]:
+#     print('element', element)
+#
+# print('after for')
+
+# def a():
+#     print('i am functiona!')
+#
+# a()
+
+# def my_sum(a, b):
+#     # print('5 + 7=', 5 + 7)
+#     # print(f'{a} + {b} = {a + b}')
+#     return a + b
+#
+# result = my_sum(1, 5)
+# print('result', result)
+# result = my_sum(-3, 8)
+# reslt = my_sum(0, 4)
+# my_sum(2.5, 5)
+# my_sum(0, 0)
+
+# def my_function(param_1):
+#     print('param_1', param_1)
+#
+# my_function(20)
+# my_function('abc')
+
