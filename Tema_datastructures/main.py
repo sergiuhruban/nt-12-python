0ac7efa9d32bb7c05024932ecbd76afb68ce145b
sergiuhruban <hrubansergiu@gmail.com>
2022-09-18 12:03:21 +0300

    Tema data structures

---
+++ Tema_datastructures/main.py
@@ -0,0 +1,11 @@
+my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+
+my_list.sort() # lista ascendenta
+print(my_list)
+
+my_list.reverse() # lista descendenta
+print(my_list)
+
+print(my_list[1::2]) # numerele pare din lista ordonata
+
+print(my_list[::2]) # numerele imparee din lista ordonata
